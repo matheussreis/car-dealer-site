9d29c7c4e031c66bc6e8b1d0a5acec42a34df3ab
Matheus Reis <53496571+matheussreis@users.noreply.github.com>
2024-07-30 19:13:01 +0100

    add next-intl localized hooks, components and functions

--- src/i18n/settings.ts
+++ src/i18n/settings.ts
@@ -1,3 +1,11 @@
+import { Pathnames } from 'next-intl/routing';
+
 export const locales = ['en', 'pt'];
 
 export const defaultLocale = 'en';
+
+export const pathnames = {
+  '/': '/',
+  '/vehicles': '/vehicles',
+  '/about': '/about',
+} satisfies Pathnames<typeof locales>;
