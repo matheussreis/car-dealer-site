280fa45c42343d71e2ef3bd7a5ee09437411486f
Matheus Reis <53496571+matheussreis@users.noreply.github.com>
2024-09-07 17:33:48 +0100

    translate the 'not found' page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,25 +1,34 @@
-import { Link } from '@/i18n/navigation';
+'use server';
 
-export default function NotFound() {
+import { Link } from '@/i18n/navigation';
+import { Metadata } from 'next';
+import { getTranslations } from 'next-intl/server';
+
+export async function generateMetadata(): Promise<Metadata> {
+  const t = await getTranslations();
+  return {
+    title: `${t('pages.notFound.title')} | ${t('site.name')}`,
+  };
+}
+
+export default async function NotFound() {
+  const t = await getTranslations('pages.notFound');
+
   return (
     <main className="flex min-h-[90dvh] flex-col items-center justify-center bg-background px-4 py-12 sm:px-6 lg:px-8">
       <div className="mx-auto max-w-md text-center">
         <div className="mx-auto h-12 w-12 text-primary" />
         <h1 className="mt-4 text-3xl font-bold tracking-tight text-foreground sm:text-4xl">
-          Oops, page not found!
+          {t('header')}
         </h1>
-        <p className="mt-4 text-muted-foreground">
-          {
-            "The page you're looking for doesn't exist. Check the URL or go back to the homepage."
-          }
-        </p>
+        <p className="mt-4 text-muted-foreground">{t('description')}</p>
         <div className="mt-6">
           <Link
             href="/"
             className="inline-flex items-center rounded-md bg-primary px-4 py-2 text-sm font-medium text-primary-foreground shadow-sm transition-colors hover:bg-primary/90 focus:outline-none focus:ring-2 focus:ring-primary focus:ring-offset-2"
             prefetch={false}
           >
-            Go to Homepage
+            {t('redirectButton')}
           </Link>
         </div>
       </div>
