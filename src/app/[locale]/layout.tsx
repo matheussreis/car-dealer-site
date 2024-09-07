c4dafe5aeb6095dd44545bc4c6acfdb14c59b4f7
Matheus Reis <53496571+matheussreis@users.noreply.github.com>
2024-09-07 17:32:06 +0100

    move next-intl context to the root layout

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,18 +1,14 @@
 import { Navbar } from '@/components/navbar';
 import { Footer } from '@/components/footer';
-import { getMessages } from 'next-intl/server';
-import { NextIntlClientProvider } from 'next-intl';
 
 export default async function AppLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
-  const messages = await getMessages();
-
   return (
-    <NextIntlClientProvider messages={messages}>
+    <>
       <Navbar />
       <main className="flex-grow">{children}</main>
       <Footer />
-    </NextIntlClientProvider>
+    </>
   );
 }
